2fd0d57cbb6fe38c5915cbe69940ca3117c48379
Shivani Chauhan <shivanichauhandev.@gmail.com>
2025-10-27 12:58:24 +0530

    modified new database

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ def create_app():
     # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://PostgresAdmin:Bua5G{nL(E@athlete-postgresql.cji80g4c8gw9.ap-south-1.rds.amazonaws.com:5432/athaispace"
 
     # app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://sport_tech_uf6r_user:5dw9fkQzcPPJJX6y8Vfyog2FWv02B1ud@dpg-d26fp28gjchc73apaae0-a.singapore-postgres.render.com:5432/sport_tech_uf6r"
-    app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://sportsdatabase_orpt_user:cGGuvoh7Z8N6160NnzOBJiAxiXTuQkAk@dpg-d3vhfbfdiees73f03e20-a.singapore-postgres.render.com/sportsdatabase_orpt"
+    app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://sportsdatabase_orpt_user:cGGuvoh7Z8N6160NnzOBJiAxiXTuQkAk@dpg-d3vhfbfdiees73f03e20-a.singapore-postgres.render.com:5432/sportsdatabase_orpt"
+    
     # Initialize the database with the app
     db.init_app(app)
     
